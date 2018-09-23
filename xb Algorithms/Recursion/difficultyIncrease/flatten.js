258f4a38eeec13ada4dc085495b02d55b047532f
NicksComingUp <remember.marsman@gmail.com>
2018-09-23 00:06:01 -0500

    finishing up the flattening recursion

--- xb Algorithms/Recursion/difficultyIncrease/flatten.js
+++ xb Algorithms/Recursion/difficultyIncrease/flatten.js
@@ -1,17 +1,19 @@
 function flatten(arr){
-
+let store = [];
 let take;
   for (let val of arr){
     // divert from the stack and go back
-    if(Array.isArray(val)){
-      // go back and flatten
-      flatten(val);
+    if(!Array.isArray(val)){
+      // store value of non array
+      store.push(val);
+      // console.log(val);
     }else{
-      // store all the values
-      take
-      console.log(val);
+      // keep pushing the value in
+    store = store.concat(flatten(val));
     }
   }
+    // console.log(store);
+  return store;
 }
 
-flatten([1,2,3,4,[9,13,5], [5,6]]);
+flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]);
